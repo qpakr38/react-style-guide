4937f19487ee0086a490b60843a9f359be320aed
qpakr38 <zczc0819@gmail.com>
2022-09-01 09:04:32 +0900

    FEAT : Styled Components ,@media 쿼리 사용

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import './Button.css';
 
 const Button = styled.button`
+    width: 100%;
     font: inherit;
     padding: 0.5rem 1.5rem;
     border: 1px solid #8b005d;
@@ -21,6 +22,10 @@ const Button = styled.button`
     border-color: #ac0e77;
     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
   }
+  @media(min-width: 768px){
+    width: auto;
+  }
+  
 `;
 
 // const Button = props => {
